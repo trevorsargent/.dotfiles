7f44cc29063c1d915c219ba92daee2f641841f90
Trevor Sargent <trevor.a.sargent@gmail.com>
2023-11-10 10:58:47 -0800

    chore: add things to autoload

---
+++ zsh/.zsh/autoload/bun.zsh
@@ -0,0 +1,2 @@
+# bun completions
+[ -s "/home/trevor/.bun/_bun" ] && source "/home/trevor/.bun/_bun"
---
+++ zsh/.zsh/autoload/gpt.zsh
@@ -0,0 +1,3 @@
+export OPENAI_KEY="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"
+
+alias c="chatgpt"
