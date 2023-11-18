a7280b1fbc71ff9e6644c1c2659d2d10661162c7
Trev <trevor.a.sargent@gmail.com>
2023-11-17 23:14:13 -0800

    Update pyenv.zsh

--- zsh/.zsh/autoload/pyenv.zsh
+++ zsh/.zsh/autoload/pyenv.zsh
@@ -1,3 +1,5 @@
-export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+if command -v pyenv >/dev/null; then
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+fi
