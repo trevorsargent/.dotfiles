7e7b041465f0329871ca718aecb979d7c48152d3
Trevor Sargent <trevor.a.sargent@gmail.com>
2026-07-16 19:24:14 -0700

    refactor(stow): flatten config packages to target ~/.config/<app>
    Packages no longer nest a .config/<app>/ mirror inside each package dir;
    files sit at the package root and install.sh stows each with an explicit
    target (stow <pkg> -t ~/.config/<pkg>).

--- install.sh
+++ install.sh
@@ -3,24 +3,23 @@
 # install dependencies
 sudo pacman -Syu --noconfirm hyprland waybar eww mako stow git alacritty
 
-# stow dotfiles
+# stow home-directory packages (files live at ~)
 stow git
 stow zsh
 stow osx
 stow conky
-stow rofi
-stow hypr
-stow waybar
-stow eww
-stow spotifyd
-stow mako -t ~/.config/mako
-stow wofi -t ~/.config/wofi
+
+# stow ~/.config packages (flat repo dirs, one per app)
+for pkg in alacritty eww hypr mako rofi spotifyd waybar wofi; do
+  mkdir -p ~/.config/$pkg
+  stow $pkg -t ~/.config/$pkg
+done
+
+# special targets
 sudo stow keyd -t /etc/keyd
 stow code -t ~/.config/Code\ -\ OSS --adopt
-stow alacritty -t ~/.config/alacritty
 # add zsh as a login shell
 command -v zsh | sudo tee -a /etc/shells
 
 # use zsh as default shell
 chsh -s $(which zsh)
-
