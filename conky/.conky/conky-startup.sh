b91e707c3375850d8e974ed87c6d9d0a5d116be6
Trevor Sargent <trevor.a.sargent@gmail.com>
2026-07-16 19:27:44 -0700

    chore: remove unused waybar, conky, and vscode configs
    eww replaced waybar as the bar, conky isn't installed anymore, and VS Code
    settings are no longer tracked (the package targeted the defunct Code - OSS
    config dir anyway). The empty zellij package dir goes with them.

--- conky/.conky/conky-startup.sh
+++
@@ -1,3 +0,0 @@
-killall conky >> /dev/null
-sleep 30s && conky -c "$HOME/.conky/trev.conkyrc" &
-
--- install.sh
+++ install.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
 # install dependencies
-sudo pacman -Syu --noconfirm hyprland waybar eww mako stow git alacritty
+sudo pacman -Syu --noconfirm hyprland eww mako stow git alacritty
 
 # stow home-directory packages (files live at ~)
 stow git
 stow zsh
 stow osx
-stow conky
 
 # stow ~/.config packages (flat repo dirs, one per app)
-for pkg in alacritty eww hypr mako spotifyd waybar wofi; do
+for pkg in alacritty eww hypr mako spotifyd wofi; do
   mkdir -p ~/.config/$pkg
   stow $pkg -t ~/.config/$pkg
 done
 
 # special targets
 sudo stow keyd -t /etc/keyd
-stow code -t ~/.config/Code\ -\ OSS --adopt
 # add zsh as a login shell
 command -v zsh | sudo tee -a /etc/shells
 
 # use zsh as default shell
 chsh -s $(which zsh)
+
